5a08f3cf4a9e34c7590ed7bd393c28952285b212
nopnop2002 <hau26280@tree.odn.ne.jp>
2021-05-28 07:22:22 +0900

    First Release

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
 # esp-idf-scp-client
-scp client example for esp-idf
+scp client example for esp-idf.   
+This project use [this](https://github.com/libssh2/libssh2) ssh library.   
+
+# Software requirements
+esp-idf v4.4 or later.   
+
+# Installation for ESP32
+
+```
+git clone https://github.com/nopnop2002/esp-idf-scp-client
+cd esp-idf-scp-client/scp-listdir
+git clone https://github.com/libssh2/libssh2 components/libssh2
+cp esp-idf/libssh2_config.h components/libssh2/include
+cp esp-idf/CMakeLists.txt components/libssh2
+idf.py set-target esp32
+idf.py menuconfig
+idf.py flash
+```
+
+# Installation for ESP32-S2
+
+```
+git clone https://github.com/nopnop2002/esp-idf-scp-client
+cd esp-idf-scp-client/scp-listdir
+git clone https://github.com/libssh2/libssh2 components/libssh2
+cp esp-idf/libssh2_config.h components/libssh2/include
+cp esp-idf/CMakeLists.txt components/libssh2
+idf.py set-target esp32s2
+idf.py menuconfig
+idf.py flash
+```
+
+# Configuration   
+You have to set this config value with menuconfig.   
+- CONFIG_ESP_WIFI_SSID   
+SSID (network name) to connect to.
+- CONFIG_ESP_WIFI_PASSWORD   
+WiFi password (WPA or WPA2) to use.
+- CONFIG_ESP_MAXIMUM_RETRY   
+Set the Maximum retry to avoid station reconnecting to the AP unlimited when the AP is really inexistent.
+- CONFIG_SSH_USER   
+Username of SSH.
+- CONFIG_SSH_PASSWORD   
+Password of SSH.
+- CONFIG_SSH_HOST   
+IP address of SSH host.   
+__mDMS name cannot be used.__   
+- CONFIG_SSH_PATH   
+Path to list.   
+
+![config-main](https://user-images.githubusercontent.com/6020549/119904361-33782700-bf85-11eb-8a90-c5f05ac83f87.jpg)
+![config-listdir](https://user-images.githubusercontent.com/6020549/119904366-3541ea80-bf85-11eb-9f85-90f93d144a35.jpg)
+
+# Examples
+- scp-listdir   
+ Obtain directory list
+
+- scp-get   
+ Get from a file
+
+- scp-put   
+ Put to a file
+
+- scp-mkdir   
+ Create new directory
---
+++ scp-listdir/main/main.c
@@ -0,0 +1,309 @@
+/* SCP read Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <string.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdio.h>
+#include <ctype.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+#include "libssh2_config.h"
+#include <libssh2.h>
+#include <libssh2_sftp.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/time.h>
+
+
+/* FreeRTOS event group to signal when we are connected*/
+static EventGroupHandle_t s_wifi_event_group;
+
+/* The event group allows multiple bits for each event, but we only care about two events:
+ * - we are connected to the AP with an IP
+ * - we failed to connect after the maximum amount of retries */
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT	   BIT1
+
+static const char *TAG = "MAIN";
+
+static int s_retry_num = 0;
+
+static void event_handler(void* arg, esp_event_base_t event_base,
+								int32_t event_id, void* event_data)
+{
+	if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+		esp_wifi_connect();
+	} else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+		if (s_retry_num < CONFIG_ESP_MAXIMUM_RETRY) {
+			esp_wifi_connect();
+			s_retry_num++;
+			ESP_LOGI(TAG, "retry to connect to the AP");
+		} else {
+			xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+		}
+		ESP_LOGI(TAG,"connect to the AP fail");
+	} else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+		ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+		ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+		s_retry_num = 0;
+		xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+	}
+}
+
+void wifi_init_sta(void)
+{
+	s_wifi_event_group = xEventGroupCreate();
+
+	ESP_ERROR_CHECK(esp_netif_init());
+
+	ESP_ERROR_CHECK(esp_event_loop_create_default());
+	esp_netif_create_default_wifi_sta();
+
+	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+	ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+	esp_event_handler_instance_t instance_any_id;
+	esp_event_handler_instance_t instance_got_ip;
+	ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+														ESP_EVENT_ANY_ID,
+														&event_handler,
+														NULL,
+														&instance_any_id));
+	ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+														IP_EVENT_STA_GOT_IP,
+														&event_handler,
+														NULL,
+														&instance_got_ip));
+
+	wifi_config_t wifi_config = {
+		.sta = {
+			.ssid = CONFIG_ESP_WIFI_SSID,
+			.password = CONFIG_ESP_WIFI_PASSWORD,
+			/* Setting a password implies station will connect to all security modes including WEP/WPA.
+			 * However these modes are deprecated and not advisable to be used. Incase your Access point
+			 * doesn't support WPA2, these mode can be enabled by commenting below line */
+		 .threshold.authmode = WIFI_AUTH_WPA2_PSK,
+
+			.pmf_cfg = {
+				.capable = true,
+				.required = false
+			},
+		},
+	};
+	ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+	ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
+	ESP_ERROR_CHECK(esp_wifi_start() );
+
+	ESP_LOGI(TAG, "wifi_init_sta finished.");
+
+	/* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
+	 * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
+	EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+			WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+			pdFALSE,
+			pdFALSE,
+			portMAX_DELAY);
+
+	/* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
+	 * happened. */
+	if (bits & WIFI_CONNECTED_BIT) {
+		ESP_LOGI(TAG, "connected to ap SSID:%s",
+				 CONFIG_ESP_WIFI_SSID);
+	} else if (bits & WIFI_FAIL_BIT) {
+		ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
+				 CONFIG_ESP_WIFI_SSID, CONFIG_ESP_WIFI_PASSWORD);
+	} else {
+		ESP_LOGE(TAG, "UNEXPECTED EVENT");
+	}
+
+	/* The event will not be processed after unregister */
+	ESP_ERROR_CHECK(esp_event_handler_instance_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, instance_got_ip));
+	ESP_ERROR_CHECK(esp_event_handler_instance_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, instance_any_id));
+	vEventGroupDelete(s_wifi_event_group);
+}
+
+#define __FILESIZE "llu"
+
+void app_main(void)
+{
+	//Initialize NVS
+	esp_err_t ret = nvs_flash_init();
+	if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+	  ESP_ERROR_CHECK(nvs_flash_erase());
+	  ret = nvs_flash_init();
+	}
+	ESP_ERROR_CHECK(ret);
+
+	ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
+	wifi_init_sta();
+
+
+	// SSH Staff
+	int sock;
+	struct sockaddr_in sin;
+	const char *fingerprint;
+	LIBSSH2_SESSION *session;
+	LIBSSH2_SFTP *sftp_session;
+	LIBSSH2_SFTP_HANDLE *sftp_handle;
+
+	ESP_LOGI(TAG, "libssh2_version is %s", LIBSSH2_VERSION);
+	int rc = libssh2_init(0);
+	if(rc) {
+		ESP_LOGE(TAG, "libssh2 initialization failed (%d)", rc);
+		while(1) { vTaskDelay(1); }
+	}
+
+	/* Ultra basic "connect to port 22 on localhost"
+	 * Your code is responsible for creating the socket establishing the
+	 * connection
+	 */
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+	if(-1 == sock) {
+		ESP_LOGE(TAG, "failed to create socket!");
+		while(1) { vTaskDelay(1); }
+	}
+
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons(22);
+	sin.sin_addr.s_addr = inet_addr(CONFIG_SSH_HOST);
+	if(connect(sock, (struct sockaddr*)(&sin),
+			   sizeof(struct sockaddr_in)) != 0) {
+		ESP_LOGE(TAG, "failed to connect!");
+		while(1) { vTaskDelay(1); }
+	}
+
+	/* Create a session instance
+	 */
+	session = libssh2_session_init();
+	if(!session) {
+		ESP_LOGE(TAG, "failed to session init");
+		while(1) { vTaskDelay(1); }
+	}
+
+	/* ... start it up. This will trade welcome banners, exchange keys,
+	 * and setup crypto, compression, and MAC layers
+	 */
+	rc = libssh2_session_handshake(session, sock);
+	if(rc) {
+		ESP_LOGE(TAG, "Failure establishing SSH session: %d", rc);
+		while(1) { vTaskDelay(1); }
+	}
+
+	/* At this point we havn't yet authenticated.  The first thing to do
+	 * is check the hostkey's fingerprint against our known hosts Your app
+	 * may have it hard coded, may go to a file, may present it to the
+	 * user, that's your call
+	 */
+	fingerprint = libssh2_hostkey_hash(session, LIBSSH2_HOSTKEY_HASH_SHA1);
+	printf("Fingerprint: ");
+	for(int i = 0; i < 20; i++) {
+		printf("%02X ", (unsigned char)fingerprint[i]);
+	}
+	printf("\n");
+
+	/* We could authenticate via password */
+	if(libssh2_userauth_password(session, CONFIG_SSH_USER, CONFIG_SSH_PASSWORD)) {
+		ESP_LOGE(TAG, "Authentication by password failed.");
+		ESP_LOGE(TAG, "Authentication username : [%s].", CONFIG_SSH_USER);
+		while(1) { vTaskDelay(1); }
+	}
+
+	/* Create sftp session */
+	sftp_session = libssh2_sftp_init(session);
+	if(!sftp_session) {
+		ESP_LOGE(TAG, "Unable to init SFTP session");
+		while(1) { vTaskDelay(1); }
+	}
+
+	/* Since we have not set non-blocking, tell libssh2 we are blocking */
+	libssh2_session_set_blocking(session, 1);
+
+	/* Request a dir listing via SFTP */
+	ESP_LOGI(TAG, "remote path=[%s]", CONFIG_SSH_PATH);
+	sftp_handle = libssh2_sftp_opendir(sftp_session, CONFIG_SSH_PATH);
+	if(!sftp_handle) {
+		ESP_LOGE(TAG, "Unable to open dir with SFTP");
+		while(1) { vTaskDelay(1); }
+	}
+
+	ESP_LOGI(TAG, "libssh2_sftp_opendir() is done, now receive listing!");
+	do {
+		char mem[512];
+		char longentry[512];
+		LIBSSH2_SFTP_ATTRIBUTES attrs;
+
+		/* loop until we fail */
+		rc = libssh2_sftp_readdir_ex(sftp_handle, mem, sizeof(mem),
+									 longentry, sizeof(longentry), &attrs);
+		if(rc > 0) {
+			/* rc is the length of the file name in the mem
+			   buffer */
+
+			if(longentry[0] != '\0') {
+				printf("%s\n", longentry);
+			}
+			else {
+				if(attrs.flags & LIBSSH2_SFTP_ATTR_PERMISSIONS) {
+					/* this should check what permissions it
+					   is and print the output accordingly */
+					printf("--fix----- ");
+				}
+				else {
+					printf("---------- ");
+				}
+
+				if(attrs.flags & LIBSSH2_SFTP_ATTR_UIDGID) {
+					printf("%4d %4d ", (int) attrs.uid, (int) attrs.gid);
+				}
+				else {
+					printf("   -	- ");
+				}
+
+				if(attrs.flags & LIBSSH2_SFTP_ATTR_SIZE) {
+					printf("%8" __FILESIZE " ", attrs.filesize);
+				}
+
+				printf("%s\n", mem);
+			}
+		}
+		else
+			break;
+
+	} while(1);
+
+	libssh2_sftp_closedir(sftp_handle);
+	libssh2_sftp_shutdown(sftp_session);
+
+	// Close a session
+	libssh2_session_disconnect(session,
+							   "Normal Shutdown, Thank you for playing");
+	libssh2_session_free(session);
+
+	// Close socket
+	close(sock);
+	ESP_LOGI(TAG, "all done");
+
+	libssh2_exit();
+
+}
